895c6476d405f2a5ea9f64438097584f2fc71d34
Alexander Mardar <amardar@ya.ru>
2021-07-31 11:27:47 +0300

    First version of the project

---
+++ js/script.js
@@ -0,0 +1,343 @@
+
+let rubIhave = document.querySelector('.rub-ihave')
+let usdIhave = document.querySelector('.usd-ihave')
+let eurIhave = document.querySelector('.eur-ihave')
+let gbpIhave = document.querySelector('.gbp-ihave')
+
+let rubIbuy = document.querySelector('.rub-ibuy')
+let usdIbuy = document.querySelector('.usd-ibuy')
+let eurIbuy = document.querySelector('.eur-ibuy')
+let gbpIbuy = document.querySelector('.gbp-ibuy')
+
+let formIhave = document.querySelector('.currency-input-ihave')
+
+let currencyIhave = "RUB";
+
+let currencyIbuy = "USD";
+
+// Получаем значение первого инпута
+
+rubIhave.setAttribute("button-on", "1")
+
+
+rubIhave.addEventListener('click', function (event) {
+   
+    rubIhave.setAttribute("button-on", "1")
+    usdIhave.setAttribute("button-on", "0")
+    eurIhave.setAttribute("button-on", "0")
+    gbpIhave.setAttribute("button-on", "0")
+
+    rubIhave.style.background = "#833AE0";
+    rubIhave.style.borderColor = "#833AE0";
+    rubIhave.style.color = "#FFFFFF";
+
+    usdIhave.style.background = "#FFFFFF";
+    usdIhave.style.borderColor = "#E5E5E5";
+    usdIhave.style.color = "#C6C6C6";
+
+    eurIhave.style.background = "#FFFFFF";
+    eurIhave.style.borderColor = "#E5E5E5";
+    eurIhave.style.color = "#C6C6C6";
+
+    gbpIhave.style.background = "#FFFFFF";
+    gbpIhave.style.borderColor = "#E5E5E5";
+    gbpIhave.style.color = "#C6C6C6";
+
+    currencyIhave = "RUB";
+
+    updateValue1Click(input1.value)
+
+})
+
+usdIhave.addEventListener('click', function (event) {
+   
+    usdIhave.setAttribute("button-on", "1")
+    rubIhave.setAttribute("button-on", "0")
+    eurIhave.setAttribute("button-on", "0")
+    gbpIhave.setAttribute("button-on", "0")
+
+    usdIhave.style.background = "#833AE0";
+    usdIhave.style.borderColor = "#833AE0";
+    usdIhave.style.color = "#FFFFFF";
+
+    rubIhave.style.background = "#FFFFFF";
+    rubIhave.style.borderColor = "#E5E5E5";
+    rubIhave.style.color = "#C6C6C6";
+    
+    eurIhave.style.background = "#FFFFFF";
+    eurIhave.style.borderColor = "#E5E5E5";
+    eurIhave.style.color = "#C6C6C6";
+
+    gbpIhave.style.background = "#FFFFFF";
+    gbpIhave.style.borderColor = "#E5E5E5";
+    gbpIhave.style.color = "#C6C6C6";
+
+    currencyIhave = "USD";
+
+    updateValue1Click(input1.value)
+    
+})
+
+eurIhave.addEventListener('click', function (event) {
+   
+    eurIhave.setAttribute("button-on", "1")
+    usdIhave.setAttribute("button-on", "0")
+    rubIhave.setAttribute("button-on", "0")
+    gbpIhave.setAttribute("button-on", "0")
+
+    eurIhave.style.background = "#833AE0";
+    eurIhave.style.borderColor = "#833AE0";
+    eurIhave.style.color = "#FFFFFF";
+
+    rubIhave.style.background = "#FFFFFF";
+    rubIhave.style.borderColor = "#E5E5E5";
+    rubIhave.style.color = "#C6C6C6";
+
+    usdIhave.style.background = "#FFFFFF";
+    usdIhave.style.borderColor = "#E5E5E5";
+    usdIhave.style.color = "#C6C6C6";
+
+    gbpIhave.style.background = "#FFFFFF";
+    gbpIhave.style.borderColor = "#E5E5E5";
+    gbpIhave.style.color = "#C6C6C6";
+
+    currencyIhave = "EUR";
+
+    updateValue1Click(input1.value)
+})
+
+gbpIhave.addEventListener('click', function (event) {
+   
+    gbpIhave.setAttribute("button-on", "1")
+    eurIhave.setAttribute("button-on", "0")
+    usdIhave.setAttribute("button-on", "0")
+    rubIhave.setAttribute("button-on", "0")
+
+    gbpIhave.style.background = "#833AE0";
+    gbpIhave.style.borderColor = "#833AE0";
+    gbpIhave.style.color = "#FFFFFF";
+
+    rubIhave.style.background = "#FFFFFF";
+    rubIhave.style.borderColor = "#E5E5E5";
+    rubIhave.style.color = "#C6C6C6";
+
+    usdIhave.style.background = "#FFFFFF";
+    usdIhave.style.borderColor = "#E5E5E5";
+    usdIhave.style.color = "#C6C6C6";
+
+    eurIhave.style.background = "#FFFFFF";
+    eurIhave.style.borderColor = "#E5E5E5";
+    eurIhave.style.color = "#C6C6C6";
+
+    currencyIhave = "GBP";
+
+    updateValue1Click(input1.value)
+    
+})
+
+
+// Получаем значение второго инпута
+
+rubIbuy.addEventListener('click', function (event) {
+   
+    rubIbuy.setAttribute("button-on", "1")
+    usdIbuy.setAttribute("button-on", "0")
+    eurIbuy.setAttribute("button-on", "0")
+    gbpIbuy.setAttribute("button-on", "0")
+
+    rubIbuy.style.background = "#833AE0";
+    rubIbuy.style.borderColor = "#833AE0";
+    rubIbuy.style.color = "#FFFFFF";
+
+    usdIbuy.style.background = "#FFFFFF";
+    usdIbuy.style.borderColor = "#E5E5E5";
+    usdIbuy.style.color = "#C6C6C6";
+
+    eurIbuy.style.background = "#FFFFFF";
+    eurIbuy.style.borderColor = "#E5E5E5";
+    eurIbuy.style.color = "#C6C6C6";
+
+    gbpIbuy.style.background = "#FFFFFF";
+    gbpIbuy.style.borderColor = "#E5E5E5";
+    gbpIbuy.style.color = "#C6C6C6";
+
+    currencyIbuy = "RUB";
+
+    updateValue2Click(input2.value)
+   
+})
+
+usdIbuy.addEventListener('click', function (event) {
+   
+    usdIbuy.setAttribute("button-on", "1")
+    rubIbuy.setAttribute("button-on", "0")
+    eurIbuy.setAttribute("button-on", "0")
+    gbpIbuy.setAttribute("button-on", "0")
+
+    usdIbuy.style.background = "#833AE0";
+    usdIbuy.style.borderColor = "#833AE0";
+    usdIbuy.style.color = "#FFFFFF";
+
+    rubIbuy.style.background = "#FFFFFF";
+    rubIbuy.style.borderColor = "#E5E5E5";
+    rubIbuy.style.color = "#C6C6C6";
+    
+    eurIbuy.style.background = "#FFFFFF";
+    eurIbuy.style.borderColor = "#E5E5E5";
+    eurIbuy.style.color = "#C6C6C6";
+
+    gbpIbuy.style.background = "#FFFFFF";
+    gbpIbuy.style.borderColor = "#E5E5E5";
+    gbpIbuy.style.color = "#C6C6C6";
+
+    currencyIbuy = "USD";
+
+    updateValue2Click(input2.value)
+
+})
+
+eurIbuy.addEventListener('click', function (event) {
+   
+    eurIbuy.setAttribute("button-on", "1")
+    usdIbuy.setAttribute("button-on", "0")
+    rubIbuy.setAttribute("button-on", "0")
+    gbpIbuy.setAttribute("button-on", "0")
+
+    eurIbuy.style.background = "#833AE0";
+    eurIbuy.style.borderColor = "#833AE0";
+    eurIbuy.style.color = "#FFFFFF";
+
+    rubIbuy.style.background = "#FFFFFF";
+    rubIbuy.style.borderColor = "#E5E5E5";
+    rubIbuy.style.color = "#C6C6C6";
+
+    usdIbuy.style.background = "#FFFFFF";
+    usdIbuy.style.borderColor = "#E5E5E5";
+    usdIbuy.style.color = "#C6C6C6";
+
+    gbpIbuy.style.background = "#FFFFFF";
+    gbpIbuy.style.borderColor = "#E5E5E5";
+    gbpIbuy.style.color = "#C6C6C6";
+
+    currencyIbuy = "EUR";
+
+    updateValue2Click(input2.value)
+
+
+})
+
+gbpIbuy.addEventListener('click', function (event) {
+   
+    gbpIbuy.setAttribute("button-on", "1")
+    eurIbuy.setAttribute("button-on", "0")
+    usdIbuy.setAttribute("button-on", "0")
+    rubIbuy.setAttribute("button-on", "0")
+
+    gbpIbuy.style.background = "#833AE0";
+    gbpIbuy.style.borderColor = "#833AE0";
+    gbpIbuy.style.color = "#FFFFFF";
+
+    rubIbuy.style.background = "#FFFFFF";
+    rubIbuy.style.borderColor = "#E5E5E5";
+    rubIbuy.style.color = "#C6C6C6";
+
+    usdIbuy.style.background = "#FFFFFF";
+    usdIbuy.style.borderColor = "#E5E5E5";
+    usdIbuy.style.color = "#C6C6C6";
+
+    eurIbuy.style.background = "#FFFFFF";
+    eurIbuy.style.borderColor = "#E5E5E5";
+    eurIbuy.style.color = "#C6C6C6";
+
+
+    currencyIbuy = "GBP";
+
+    updateValue2Click(input2.value)
+
+    
+})
+
+// Отслаживаем ввод формы 1/2 и определяем значение первой переменной
+
+const input1 = document.querySelector('.first-box');
+const input2 = document.querySelector('.second-box');
+
+input1.addEventListener('input', updateValue1);
+input2.addEventListener('input', updateValue2);
+
+function updateValue1(e) {
+
+    fetch(`https://api.exchangerate.host/latest?base=${currencyIhave}&symbols=${currencyIbuy}`)
+    .then(response => response.json())
+    .then(data => {data.rates[currencyIbuy];
+  
+     let converter = data.rates[currencyIbuy];
+  
+     console.log(converter)
+
+     console.log(currencyIhave)
+  
+     input2.value = e.target.value * converter;
+     
+    });
+}
+
+function updateValue2(e) {
+    fetch(`https://api.exchangerate.host/latest?base=${currencyIbuy}&symbols=${currencyIhave}`)
+    .then(response => response.json())
+    .then(data => {data.rates[currencyIhave];
+  
+     let converter = data.rates[currencyIhave];
+  
+     console.log(converter)
+  
+     input1.value = e.target.value * converter;
+
+    });
+
+}
+
+// Прописываем расчеты при переключении кнопок
+
+
+function updateValue1Click(e) {
+
+    fetch(`https://api.exchangerate.host/latest?base=${currencyIhave}&symbols=${currencyIbuy}`)
+    .then(response => response.json())
+    .then(data => {data.rates[currencyIbuy];
+  
+     let converter = data.rates[currencyIbuy];
+  
+     console.log(converter)
+
+     console.log(currencyIhave)
+  
+     input2.value = e * converter;
+
+     bottomCurs1.innerText = `1 ${currencyIhave} = ${converter} ${currencyIbuy} `
+     bottomCurs2.innerText = `1 ${currencyIbuy} = ${converter} ${currencyIhave} `
+    });
+
+}
+
+function updateValue2Click(e) {
+    fetch(`https://api.exchangerate.host/latest?base=${currencyIbuy}&symbols=${currencyIhave}`)
+    .then(response => response.json())
+    .then(data => {data.rates[currencyIhave];
+  
+     let converter = data.rates[currencyIhave];
+  
+     console.log(converter)
+  
+     input1.value = e * converter;
+
+     bottomCurs2.innerText = `1 ${currencyIbuy} = ${converter} ${currencyIhave} `
+
+    });
+
+}
+
+// Прописываем логику нижней плашки
+
+const bottomCurs1 = document.querySelector('.bottom-label');
+const bottomCurs2 = document.querySelector('.bottom-labe2');
